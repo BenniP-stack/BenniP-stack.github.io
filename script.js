da2d48dfa48d4811f42776728d9458d298de22cd
BenniP <benjamin.reinhold.pipus@hs-hurtwangen.de>
2022-05-06 16:39:23 +0200

    gsap animation testing

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const intro = document.querySelector('.intro');
 const video = intro.querySelector('video');
 const text = intro.querySelector('h1');
+
 //END SEC
 const section = document.querySelector('section');
 const end = section.querySelector('h1');
@@ -9,25 +10,13 @@ const end = section.querySelector('h1');
 const controller = new ScrollMagic.Controller();
 
 //Scenes
-let scene = new ScrollMagic.Scene({
-        duration: 10000,
+let heroVideo = new ScrollMagic.Scene({
+        duration: 3500,
         triggerElement: intro,
         triggerHook: 0
     })
-    //.addIndicators()
+    .addIndicators()
     .setPin(intro)
-    .addTo(controller);
-
-
-//Text animation
-const textAnim = gsap.fromTo(text, 2, { opacity: 1 }, { opacity: 0 });
-
-let scene2 = new ScrollMagic.Scene({
-        duration: 3000,
-        triggerElement: intro,
-        triggerHook: 0
-    })
-    .setTween(textAnim)
     .addTo(controller);
 
 //Vid Animation
@@ -35,7 +24,7 @@ let accelAmount = 0.1;
 let scrollpos = 0;
 let delay = 0;
 
-scene.on('update', e => {
+heroVideo.on('update', e => {
     scrollpos = e.scrollPos / 1000;
 });
 
@@ -43,3 +32,21 @@ setInterval(() => {
     delay += (scrollpos - delay) * accelAmount;
     video.currentTime = delay;
 }, 33.3); //Framerate "Fix"
+
+
+//Text animation
+const textAnim = gsap.fromTo(text, 2, { opacity: 1 }, { opacity: 0 });
+
+let heroText = new ScrollMagic.Scene({
+        duration: 1000,
+        triggerElement: intro,
+        triggerHook: 0
+    })
+    .setTween(textAnim)
+    .addTo(controller);
+
+let test1 = new ScrollMagic.Scene({
+        triggerElement: '#main'
+    })
+    .setClassToggle('.render', 'fade-in')
+    .addTo(controller);
