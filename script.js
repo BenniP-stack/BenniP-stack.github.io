f8772dd098a5eddb4520d16bd174dfe07e5e7f59
BenniP <benjamin.reinhold.pipus@hs-hurtwangen.de>
2022-06-15 19:05:24 +0200

    progress
    footer, some stuff

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 const intro = document.querySelector(".intro");
 const video = intro.querySelector("video");
 const text = intro.querySelector("h1");
+const smallTxt = intro.querySelector("p");
+const introImg = intro.querySelector("img");
 // const vid2Div = document.querySelector(".loop1");
 // const vid2 = vid2Div.querySelector("video");
 
-
-//END SEC
-// const section = document.querySelector('section');
-// const end = section.querySelector('h1');
 
 //SCROLL
 const controller = new ScrollMagic.Controller();
 
 //---
-
 let accelAmount = 0.1;
 let scrollpos = 0;
-let delay = 0.01;
+let delay = 0.2;
 
 //Scenes
 let heroVideo = new ScrollMagic.Scene({
-        duration: 3500,
+        duration: 18000,
         triggerElement: intro,
         triggerHook: 0
     })
@@ -36,31 +33,15 @@ heroVideo.on('update', e => {
 setInterval(() => {
     delay += (scrollpos - delay) * accelAmount;
     video.currentTime = delay;
-}, 33.3); //Framerate "Fix"
+}, 25); //Framerate "Fix"
 
 //------
-
-// let loopVid = new ScrollMagic.Scene({
-//         duration: 2000,
-//         triggerElement: vid2Div,
-//         triggerHook: 0
-//     })
-//     .addIndicators({ name: 'VideoScroll', colorEnd: '#000000' })
-//     .setPin(vid2Div)
-//     .addTo(controller);
-
-
-// loopVid.on('update', e => {
-//     scrollpos = e.scrollPos / 1000;
-// });
-
-// setInterval(() => {
-//     delay += (scrollpos - delay) * accelAmount;
-//     vid2.currentTime = delay;
-// }, 41.6); //runtime:fps
+//TODO fix the 2nd vid bro, alt: do a lil parallax ting w the render and ScrollTrigger (Airpods)
 
 //Text animation
 const textAnim = gsap.fromTo(text, 2, { opacity: 1 }, { opacity: 0 });
+const textAnim2 = gsap.fromTo(smallTxt, 2, { opacity: 1 }, { opacity: 0 });
+const imgAnim = gsap.fromTo(introImg, 2, { opacity: 1 }, { opacity: 0 });
 
 let heroText = new ScrollMagic.Scene({
         duration: 1000,
@@ -70,6 +51,21 @@ let heroText = new ScrollMagic.Scene({
     .setTween(textAnim)
     .addTo(controller);
 
+let smallTxtScene = new ScrollMagic.Scene({
+        duration: 1000,
+        triggerElement: intro,
+        triggerHook: 0
+    })
+    .setTween(textAnim2)
+    .addTo(controller);
+
+let imgScene = new ScrollMagic.Scene({
+        duration: 1000,
+        triggerElement: intro,
+        triggerHook: 0
+    })
+    .setTween(imgAnim)
+    .addTo(controller);
 
 //parallax timeline animation
 // let timeline = new TimelineMax();
